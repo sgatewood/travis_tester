5dec2d344b19fc0673e430012aa21e7a0876064a
Sean Gatewood <stg2bd@virginia.edu>
2019-04-08 01:23:25 -0400

    gh

--- yay/tests.py
+++ yay/tests.py
@@ -9,9 +9,17 @@ from django.urls import reverse
 from django.test.utils import override_settings
 from django.conf import settings
 
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 class SeleniumTest(LiveServerTestCase):
 
+
+
     def setUp(self):
+        cap = DesiredCapabilities().FIREFOX
+        cap["marionette"] = False
+        browser = webdriver.Firefox(capabilities=cap, executable_path="geckodriver")
+
         self.browser = webdriver.Firefox()
         super(SeleniumTest, self).setUp()
 
@@ -19,4 +27,4 @@ class SeleniumTest(LiveServerTestCase):
         self.browser.get(self.live_server_url+url)
 
     def test_yay(self):
-        self.load("")
+        self.load("/pasta")
